e9afa7b9e24ef0e4d4c4c26301ceac3b30b3dc21
jukhg10 <jukhg10@gmail.com>
2025-04-26 13:01:21 -0500

    Initial commit - Upload RolesApi backend

---
+++ Models/Role.cs
@@ -0,0 +1,8 @@
+﻿namespace RolesApi.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }   // Unique identifier
+        public string Name { get; set; }  // Role name (example: "Admin", "Student")
+    }
+}
---
+++ Models/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace RolesApi.Models
+{
+    public class UserRole
+    {
+        public int UserId { get; set; }   // User Identifier
+        public int RoleId { get; set; }   // Linked Role Identifier
+    }
+}
